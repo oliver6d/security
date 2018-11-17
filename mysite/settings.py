99ce1ee24eb770160446ff4150527a02a441827b
Olive <oliver6d@gmail.com>
2018-11-17 03:51:54 -0500

    deployment changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '35(b&#42s45wpw7u@*ml_ks(j!p&5kec@ixh#mpv=-9i$+$_ov'
+## SECRET_KEY = '35(b&#42s45wpw7u@*ml_ks(j!p&5kec@ixh#mpv=-9i$+$_ov'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+## DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+
 
 ALLOWED_HOSTS = []
 
